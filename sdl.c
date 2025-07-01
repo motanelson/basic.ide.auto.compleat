853b4b8143de11e982b45008b275f41ac760f597
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-07-01 04:08:29 +0100

    Add files via upload

--- sdl.c
+++ sdl.c
@@ -3,12 +3,17 @@
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <time.h>
 #include <math.h>
 #include <SDL/SDL.h>
 #include <SDL_image.h>
 
 int main() {
   SDL_Color color;
+  SDL_Rect outlineRect= { 100, 100, 100, 100 };
   color.r=255;
   color.g=255;
   color.b=0;
@@ -18,11 +23,17 @@ int main() {
 #ifdef TEST_SDL_LOCK_OPTS
   EM_ASM("SDL.defaults.copyOnLock = false; SDL.defaults.discardOnLock = true; SDL.defaults.opaqueFrontBuffer = false;");
 #endif
-  SDL_Rect outlineRect = { 100, 100, 100, 100 };
+    
+  for(int i=100;i<500;i=i+10){
+     outlineRect.y=i;
+     outlineRect.x=i;
           
-  SDL_FillRect( screen, &outlineRect ,SDL_MapRGB(screen->format, color.r, color.g, color.b));
+     SDL_FillRect( screen, &outlineRect ,SDL_MapRGB(screen->format, color.r, color.g, color.b));
  
-  SDL_Flip(screen);
+     SDL_Flip(screen);
+     
+
+  }
   printf("\n\nend\n ");
   SDL_Quit();
   
